4cd622694a8f46b703c2c0428ac6c401f4f2e030
Sxmen07 <sxmen07@gmail.com>
2026-04-22 22:35:39 +0800

    Updated entire filed

--- StudentManagementSystem/Admin/AdminProfile.aspx.cs
+++ StudentManagementSystem/Admin/AdminProfile.aspx.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace StudentManagementSystem
+namespace StudentManagementSystem.Admin
 {
-    public partial class _Default : Page
+    public partial class AdminProfille : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
