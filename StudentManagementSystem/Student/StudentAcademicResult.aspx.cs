a5a4e5c70ca5bc4f308019f48448381d56725fa5
kaijie-nothere <kjie455@gmail.com>
2026-06-10 00:29:14 +0800

    Revert "Delete StudentManagementSystem/Student directory"
    This reverts commit aa89109d186bc51a3fbe909bfc8f8a18c3534853.

---
+++ StudentManagementSystem/Student/StudentAcademicResult.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StudentManagementSystem.Student
+{
+    public partial class StudentAcademicResult : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
