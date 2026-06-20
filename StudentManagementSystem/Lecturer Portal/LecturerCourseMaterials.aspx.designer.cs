e2af2ae40ce750b01cc9eace7aeb4f12e53b9447
alvan0726 <p22014614@student.newinti.edu.my>
2026-06-20 22:48:22 +0800

    Update LecturerCourseMaterials.aspx.designer.cs

--- StudentManagementSystem/Lecturer Portal/LecturerCourseMaterials.aspx.designer.cs
+++ StudentManagementSystem/Lecturer Portal/LecturerCourseMaterials.aspx.designer.cs
@@ -114,6 +114,15 @@ namespace LecturerPortal
         protected global::System.Web.UI.WebControls.TextBox txtDescription;
 
         /// <summary>
+        /// ddlCategory control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlCategory;
+
+        /// <summary>
         /// fileUpload control.
         /// </summary>
         /// <remarks>
