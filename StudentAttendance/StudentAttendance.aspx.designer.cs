f294d47a4d1512b95aa82b4ea8cc8885c2e8572c
Sxmen07 <76441250+Sxmen07@users.noreply.github.com>
2026-06-21 23:45:15 +0800

    Update StudentAttendance.aspx.designer.cs

--- StudentAttendance/StudentAttendance.aspx.designer.cs
+++ StudentAttendance/StudentAttendance.aspx.designer.cs
@@ -15,6 +15,42 @@ namespace StudentManagementSystem.Student
     {
 
         /// <summary>
+        /// lblOverallAttendance control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblOverallAttendance;
+
+        /// <summary>
+        /// lblTotalPresent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTotalPresent;
+
+        /// <summary>
+        /// lblTotalLate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTotalLate;
+
+        /// <summary>
+        /// lblTotalAbsent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTotalAbsent;
+
+        /// <summary>
         /// btnCurrent control.
         /// </summary>
         /// <remarks>
